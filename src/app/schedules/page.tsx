3241934219b3862c1e7fc9a573bdd04fc870de8e
yoouung <jyp101400@gmail.com>
2024-12-23 16:32:14 +0900

    fix: 플로팅 버튼 위치 수정

--- src/app/schedules/page.tsx
+++ src/app/schedules/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   const planList = courseData.courses
 
   return (
-    <div className='w-full pt-[20px] pb-[32px] px-[16px] flex flex-col'>
+    <div className='w-full pt-[20px] pb-[20px] px-[16px] flex flex-col'>
       <span className='border-b text-[18px] inline-flex items-center'>
         <p className='font-bold'>{userName}</p>
         <p>님의 코스 플랜</p>
@@ -22,7 +22,7 @@ export default function Page() {
         <CourseCard key={index} courseData={course} />
       ))}
 
-      <div className='fixed bottom-[60px] right-[20px] flex items-center justify-center cursor-pointer z-[50]'>
+      <div className='sticky bottom-[60px] flex items-center justify-end cursor-pointer z-[50]'>
         <button
           className='w-[50px] h-[50px] bg-white rounded-full flex items-center justify-center shadow-lg border border-blue-800 border-opacity-20'
           onClick={() => router.push('/schedules/new')}
