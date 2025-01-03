911aadbf1ed177f0ff69c0f3569c21679e268c29
erica0321 <hyunahrho29@gmail.com>
2025-01-03 20:02:14 +0900

    fix: header, footer 변경

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,55 +3,58 @@
 import Image from 'next/image'
 import home from '@images/home.png'
 import course from '@images/course.png'
-import calendar from '@images/calendar.png'
+import plan from '@images/plan.png'
+import planPurple from '@images/plan_color.png'
 import my from '@images/my.png'
 import homePurple from '@images/home_color.png'
 import coursePurple from '@images/course_color.png'
 import myPurple from '@images/my_color.png'
-import calendaPurple from '@images/calendar_color.png'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 export default function Footer() {
   const path = usePathname()
+  const isHome = path === '/'
+  const isCourse = path.includes('/courses')
+  const isPlan = path.includes('/schedules')
+  const isMy = path.includes('/users')
 
   return (
-    <footer className='fixed bottom-0 shadow-custom max-w-[375px] py-2 text-gray-400 text-base bg-white flex w-full h-50 justify-around items-center'>
-      <Link href='/' className='flex flex-col items-center'>
+    <footer
+      className={
+        'fixed bottom-0 shadow-custom max-w-[375px] py-2 text-gray-400 text-base bg-white flex w-full h-50 justify-around items-center'
+      }
+    >
+      <Link href='/' className={'flex flex-col items-center'}>
         <Image
-          src={path === '/' ? homePurple : home}
+          src={isHome ? homePurple : home}
           width={25}
           height={25}
           alt='홈'
         />
-        홈
+        <span className={`text-[10px] ${isHome && 'text-brand'}`}>홈</span>
       </Link>
-      <Link href='/courses' className='flex flex-col items-center'>
+      <Link href='/courses' className={'flex flex-col items-center'}>
         <Image
-          src={path.includes('/courses') ? coursePurple : course}
+          src={isCourse ? coursePurple : course}
           width={25}
           height={25}
           alt='코스'
         />
-        코스
+        <span className={`text-[10px] ${isCourse && 'text-brand'}`}>코스</span>
       </Link>
-      <Link href='/schedules' className='flex flex-col items-center'>
+      <Link href='/schedules' className={'flex flex-col items-center'}>
         <Image
-          src={path.includes('/schedules') ? calendaPurple : calendar}
+          src={isPlan ? planPurple : plan}
           width={25}
           height={25}
           alt='플랜'
         />
-        플랜
+        <span className={`text-[10px] ${isPlan && 'text-brand'}`}>플랜</span>
       </Link>
-      <Link href='/users/1' className='flex flex-col items-center'>
-        <Image
-          src={path.includes('/users') ? myPurple : my}
-          width={25}
-          height={25}
-          alt='마이'
-        />
-        마이
+      <Link href='/users/1' className={'flex flex-col items-center'}>
+        <Image src={isMy ? myPurple : my} width={25} height={25} alt='마이' />
+        <span className={`text-[10px] ${isMy && 'text-brand'}`}>마이</span>
       </Link>
     </footer>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        brand: '#5A59F2',
       },
       spacing: {
         10: '10px',
