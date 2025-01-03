e6c29842013f37af6543bbb36d55aa06d716ffc5
erica0321 <hyunahrho29@gmail.com>
2025-01-03 21:42:35 +0900

    feat: splash 느낌으로 suspense 적용

---
+++ src/app/login/components/Splash.tsx
@@ -0,0 +1,38 @@
+import splashLogo from '@images/splash_logo.svg'
+import Image from 'next/image'
+import logo_long from '@images/logo_long.png'
+
+export default function Splash() {
+  return (
+    <div
+      className={
+        'w-full h-full bg-brand-blue flex flex-col items-center justify-center'
+      }
+    >
+      <Image
+        className={'w-full h-full absolute top-[-90px]'}
+        src={splashLogo}
+        alt='splash'
+        width={100}
+        height={100}
+      />
+      <div
+        className={
+          'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'
+        }
+      >
+        <p className={'text-center text-[22px] font-bold'}>공유하고</p>
+        <p className={'text-center text-[22px] font-bold'}>공유받는</p>
+        <p className={'text-center text-[22px] font-bold'}>우리들의</p>
+        <p className={'text-center text-[22px] font-bold'}>코스</p>
+      </div>
+      <Image
+        className={'absolute bottom-[50px] left-1/2 transform -translate-x-1/2'}
+        src={logo_long}
+        alt='logo_long'
+        width={100}
+        height={100}
+      />
+    </div>
+  )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react'
+import Splash from './components/Splash'
+import Login from './components/Login'
+
+export default function Page() {
+  return (
+    <div className={'w-full h-full bg-brand'}>
+      <Suspense fallback={<Splash />}>
+        <div
+          className={'w-full h-full flex flex-col items-center justify-center'}
+        >
+          <Login />
+        </div>
+      </Suspense>
+    </div>
+  )
+}
