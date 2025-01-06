bb4e441e5ba0122889b4d576c9a837192161115f
yoouung <jyp101400@gmail.com>
2025-01-06 17:55:22 +0900

    rename: 레이아웃 컴포넌트 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import LayoutSpacer from '@components/LayoutSpacer'
+import Header from '@components/(layout)/Header'
+import Footer from '@components/(layout)/Footer'
+import LayoutSpacer from '@components/(layout)/LayoutSpacer'
 
 export const metadata: Metadata = {
   title: 'WOOCO - 우코',
--- src/app/login/components/Login.tsx
+++ src/app/login/components/Login.tsx
@@ -1,4 +1,4 @@
-import Spacer from '@/app/components/Spacer'
+import Spacer from '@components/(layout)/Spacer'
 import Image from 'next/image'
 import splashLogo from '@images/splash_logo.svg'
 
