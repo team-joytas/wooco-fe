a033b61ceb063b9ad8f885689034c463e9446e2b
yoouung <jyp101400@gmail.com>
2025-01-09 22:42:22 +0900

    feat: 지역 추가 페이지 구현
    - 지역 검색 Cascader 컴포넌트 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@components/(layout)/Footer'
 import LayoutSpacer from '@components/(layout)/LayoutSpacer'
 import localFont from 'next/font/local'
 import { AnimatePresence } from 'framer-motion'
+import { ConfigProvider } from 'antd'
 
 export const metadata: Metadata = {
   title: 'WOOCO - 우코',
@@ -21,6 +22,17 @@ const pretendard = localFont({
   variable: '--font-pretendard',
 })
 
+const theme = {
+  token: {
+    borderRadius: 20,
+    colorPrimary: '#5A59F2',
+    colorBgContainer: '#F7F7F7',
+    colorBorder: '#ffffff',
+    colorText: 'rgba(0, 0, 0, 0.5)',
+    fontSize: 11,
+  },
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -30,12 +42,14 @@ export default function RootLayout({
     <html lang='kr' className={`h-vh ${pretendard.variable}`}>
       <body className='h-full flex items-center flex-col overflow-y-scroll'>
         <Header />
-        <AnimatePresence>
-          <div className='mx-auto flex-1 text-black w-full max-w-[375px]'>
-            {children}
-            <LayoutSpacer />
-          </div>
-        </AnimatePresence>
+        <ConfigProvider theme={theme}>
+          <AnimatePresence>
+            <div className='mx-auto flex-1 text-black w-full max-w-[375px]'>
+              {children}
+              <LayoutSpacer />
+            </div>
+          </AnimatePresence>
+        </ConfigProvider>
         <Footer />
       </body>
     </html>
