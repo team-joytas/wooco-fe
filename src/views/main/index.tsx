1d4af154bd3102c7db5bc7c57c564628c1d68675
erica0321 <hyunahrho29@gmail.com>
2025-01-18 01:10:26 +0900

    fix: 메인 수정

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,5 +1,3 @@
-import SectionAnnouncement from '@/src/features/main/section-announcement'
-import SectionEvent from '@/src/features/main/section-event'
 import SectionBanner from '@/src/features/main/section-banner'
 import SectionNewCourse from '@/src/features/main/section-new-course'
 import SectionLikeCourse from '@/src/features/main/section-like-course'
@@ -13,11 +11,24 @@ export default function Main() {
       <SectionNewCourse />
       <Spacer className='bg-light-gray' height={8} />
       <SectionLikeCourse />
-      <SectionBanner.News />
-      <SectionEvent />
-      <Spacer className='bg-light-gray' height={8} />
-      <SectionAnnouncement />
       <SectionBottom />
     </div>
   )
 }
+
+/* TODO: 공지사항, 이벤트 API 완료 시 추가
+function ArchiveMain() {
+  return (
+    <div className='flex items-center flex-col h-[calc(100%-60px)]'>
+      <SectionBanner.Course />
+      <SectionNewCourse />
+      <Spacer className='bg-light-gray' height={8} />
+      <SectionLikeCourse />
+      <SectionBanner.News />
+      <SectionEvent />
+      <Spacer className='bg-light-gray' height={8} />
+      <SectionAnnouncement />
+    </div>
+  )
+}
+*/
