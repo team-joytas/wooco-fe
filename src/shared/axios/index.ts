0c11e3373e27920024219777fc1aeb5e762faa4d
erica0321 <hyunahrho29@gmail.com>
2025-01-30 13:47:52 +0900

    feat: reissue 로직 추가

--- src/shared/axios/index.ts
+++ src/shared/axios/index.ts
@@ -18,6 +18,43 @@ customAxios.interceptors.request.use((config) => {
   return config
 })
 
+customAxios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config
+    const accessToken = localStorage.getItem('accessToken')
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      accessToken
+    ) {
+      originalRequest._retry = true
+
+      try {
+        const { data } = await axios.post(
+          `${SERVER_URL}/api/v1/auth/reissue`,
+          {},
+          {
+            withCredentials: true,
+            headers: { 'Content-Type': 'application/json' },
+          }
+        )
+
+        if (data.accessToken) {
+          localStorage.setItem('accessToken', data.accessToken)
+          originalRequest.headers.Authorization = `Bearer ${data.accessToken}`
+          return customAxios(originalRequest)
+        }
+      } catch (reissueError) {
+        localStorage.removeItem('accessToken')
+        window.location.href = '/login'
+        return Promise.reject(reissueError)
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
 export const publicAxios: AxiosInstance = axios.create({
   baseURL: `${SERVER_URL}/api/v1`,
   headers: {
