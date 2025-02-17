65c2f730d9cecc4cfa09cd099eff279141157a22
yoouung <jyp101400@gmail.com>
2025-02-17 23:44:45 +0900

    feat: HelperText 컴포넌트 추가

---
+++ src/shared/ui/HelperText.tsx
@@ -0,0 +1,7 @@
+export default function HelperText({ message }: { message: string }) {
+  return (
+    <span className='text-[10px] pl-[10px] text-red-500 mt-[-5px]'>
+      {message}
+    </span>
+  )
+}
