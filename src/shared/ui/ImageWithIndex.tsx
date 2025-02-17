427e1e82e3a970f7310c9008d2afdcfe098b71f7
yoouung <jyp101400@gmail.com>
2025-02-17 23:15:52 +0900

    design: 인덱스 표시 이미지 스타일 수정

--- src/shared/ui/ImageWithIndex.tsx
+++ src/shared/ui/ImageWithIndex.tsx
@@ -7,13 +7,13 @@ interface ImageWithIndexProps {
 
 export default function ImageWithIndex({ src, index }: ImageWithIndexProps) {
   return (
-    <div className='w-[60px] h-[58px] relative  border border-container-light-blue flex items-center justify-center rounded-[5px] flex-shrink-0'>
+    <div className='w-[60px] h-[58px] relative flex items-center justify-center rounded-[5px] flex-shrink-0'>
       <Image
         alt='place'
         width={52}
         height={52}
         src={src || logo}
-        className='w-[52px] h-[52px] rounded-[5px] object-cover'
+        className='w-[52px] h-[52px] rounded-[5px] object-cover border border-container-light-blue'
         layout='fixed'
       />
       <div className='absolute top-0 left-0 w-[13px] h-[13px] text-white bg-container-light-blue rounded-full text-[8px] flex items-center justify-center'>
