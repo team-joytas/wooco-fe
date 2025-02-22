5c5a50e929f72ffd1a853dd3b61d166cd0b04cf5
yoouung <jyp101400@gmail.com>
2025-02-22 18:14:45 +0900

    fix: 안쓰는 코드 주석처리

--- src/entities/user/type.ts
+++ src/entities/user/type.ts
@@ -1,27 +1,27 @@
 import type { PlaceType } from '@/src/entities/place/type'
 import type { CourseType } from '@/src/entities/course/type'
 
-export type UserType = {
-  user_info: {
-    user_id: number
-    name: string
-    profile_url: string
-    description?: string
-  }
-  place_info: {
-    summary: {
-      star_rate_avg: number
-      total_place: number
-    }
-    places: PlaceType[]
-  }
-  course_info: {
-    summary: {
-      total_course: number
-    }
-    courses: CourseType[]
-  }
-}
+// export type UserType = {
+//   user_info: {
+//     user_id: number
+//     name: string
+//     profile_url: string
+//     description?: string
+//   }
+//   place_info: {
+//     summary: {
+//       star_rate_avg: number
+//       total_place: number
+//     }
+//     places: PlaceType[]
+//   }
+//   course_info: {
+//     summary: {
+//       total_course: number
+//     }
+//     courses: CourseType[]
+//   }
+// }
 
 export type UserProfileType = {
   name: string
