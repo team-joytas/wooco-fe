4e9f886860d9ee578053f7110a77e6f4aa6aece4
yoouung <jyp101400@gmail.com>
2025-02-22 19:56:42 +0900

    fix: 이미지 커스텀 로더 추가

--- src/shared/ui/ProfileImage.tsx
+++ src/shared/ui/ProfileImage.tsx
@@ -11,6 +11,10 @@ export default function ProfileImage({
   className?: string
   type?: 'colored'
 }) {
+  const customLoader = ({ src }: { src: string }) => {
+    return src
+  }
+
   if (type === 'colored') {
     return (
       <div
@@ -19,6 +23,7 @@ export default function ProfileImage({
         }px] bg-gradient-to-r from-[#9997F2] to-[#4341EA] p-[1px] rounded-[50%]`}
       >
         <Image
+          loader={customLoader}
           className={`w-[${size}px] h-[${size}px] bg-white rounded-full border-gray border-[0.5px] object-cover ${className}`}
           alt='프로필이미지'
           src={src}
@@ -31,11 +36,13 @@ export default function ProfileImage({
 
   return (
     <Image
+      loader={customLoader}
       className={`w-[${size}px] h-[${size}px] rounded-full border-gray border-[0.5px] object-cover ${className}`}
       alt='프로필이미지'
       src={src}
       width={size}
       height={size}
+      quality={100}
     />
   )
 }
