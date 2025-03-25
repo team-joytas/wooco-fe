7d58b5a6415a5b68fd5cfc0adbeb105b27a19d36
HidenLee <hyundo1995@gmail.com>
2025-03-25 18:47:38 +0900

    feat: 마이페이지 UI 구성 변경

--- src/features/user/user-profile-section.tsx
+++ src/features/user/user-profile-section.tsx
@@ -1,33 +1,36 @@
 import type { UserProfileType } from '@/src/entities/user'
 import ProfileImage from '@/src/shared/ui/ProfileImage'
-import UserTag from '@/src/features/user/user-tag'
-import { useRouter } from 'next/navigation'
+import { Heart, ChevronRight } from 'lucide-react'
 
 export default function UserProfileSection({
   user,
 }: {
   user: UserProfileType | undefined
 }) {
-  const router = useRouter()
+  // TODO: 매직 넘버 API 연결로 대체 예정
+  const COUNT_PREFERENCE_COURSES = 23
 
   return (
-    <section className='px-[20px] py-[10px] gap-[5px] w-full flex flex-col justify-between'>
-      <div className='flex items-center justify-between'>
-        <div className='flex flex-col justify-center items-start gap-[10px]'>
+    <section className='p-2.5 gap-[6px] w-full flex flex-col justify-between'>
+      <div className='flex flex-row items-center justify-start gap-[16px]'>
+        <div className='flex justify-center items-start gap-[10px]'>
           <ProfileImage size={60} src={user?.profile_url || '/profile.png'} />
-          <p className='font-bold text-brand text-headline'>{user?.name}</p>
         </div>
-        <div className='flex gap-[30px] items-end'>
-          <UserTag
-            type='heart'
-            content={'?'}
-            onClick={() => router.push(`/users/${user?.user_id}/wishlist`)}
-          />
-          <UserTag type='comment' content={'?'} />
-          <UserTag type='rate' content={'?'} />
+        <div className='flex flex-col items-start justify-center'>
+          <p className='font-bold text-brand text-headline'>{user?.name}</p>
+          <p className='text-middle font-medium'>{user?.description}</p>
         </div>
       </div>
-      <span className='text-sub font-light'>{user?.description}</span>
+      <div className='flex flex-row px-[10px] py-[13px] items-center justify-between w-[346px] h-[50px] rounded-[10px] bg-light-gray'>
+        <p className='text-middle font-medium'>관심 코스 목록</p>
+        <div className='flex flex-row justify-center items-center gap-[6px]'>
+          <div className='flex flex-row gap-[4px]'>
+            <Heart className='fill-brand stroke-0 mt-[1.5px]' size={20} />
+            {COUNT_PREFERENCE_COURSES}
+          </div>
+          <ChevronRight className='text-description' size={24} />
+        </div>
+      </div>
     </section>
   )
 }
