e912e546a2f63d88f7c942921fa7a1d228a30e18
HidenLee <hyundo1995@gmail.com>
2025-03-26 07:14:55 +0900

    design: 장소 검색결과 디자인 반영

--- src/views/search-place/index.tsx
+++ src/views/search-place/index.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { Input } from 'antd'
 import Spacer from '@/src/shared/ui/Spacer'
 import { CoursePlanPlaceType, PlaceSearchType } from '@/src/entities/place'
 import { Dispatch, SetStateAction } from 'react'
 import Header from '@/src/widgets/header'
 import { getPlaceSearchResult, postPlace } from '@/src/entities/place'
-
+import { Search } from 'lucide-react'
 type MetaType = {
   total_count: number
   pageable_count: number
@@ -66,37 +65,48 @@ export default function SearchPlace({
       <Header title='장소 추가하기' close={() => setOpenSearchPlace(false)} />
       <Spacer height={18} />
       <div className='px-[20px] w-full flex flex-col gap-[15px]'>
-        <div className='flex flex-col gap-[10px]'>
-          <span className='text-main font-semibold'>장소명</span>
-          <Input.Search
-            placeholder='장소 이름을 입력해주세요.'
-            className='w-full'
-            value={inputValue}
-            onSearch={() => getResult(inputValue)}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
+        <div className="flex flex-col gap-[15px]">
+          <span className="text-main font-semibold">장소명</span>
+          <div className="flex items-center justify-center ">
+            <div
+              className="w-[304px] h-[36px] px-[14px] py-[10px] relative rounded-[2025px] bg-bright-gray flex flex-row items-center justify-start box-border gap-[10px]">
+              <Search width={14} size={14} />
+              <input
+                className="relative font-medium text-middle text-black bg-transparent outline-0"
+                placeholder="장소 이름을 입력해주세요."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={e => {
+                  if (e.nativeEvent.isComposing) return
+                  if (e.key === 'Enter') {
+                    getResult(inputValue)
+                  }
+                }}
+              />
+            </div>
+          </div>
+          <Spacer height={8} className="bg-bright-gray opacity-50" />
         </div>
-        <div className='w-full mt-[20px] h-[2px] bg-gray-100' />
-        <div className='justify-start items-center overflow-y-auto h-[calc(100vh-200px)]'>
-          <div className='h-fit w-full flex flex-col gap-[5px]'>
+        <div className=" overflow-y-auto h-[calc(100vh-200px)]">
+          <div className="h-fit w-full flex flex-col gap-[9px] pt-1 justify-center items-center">
             {results.length > 0 ? (
               results.map((result) => {
                 return (
                   <div
                     key={result.id}
-                    className='text-[12px] border-blue-100 rounded-[5px] border-[1px] flex flex-col w-full gap-[5px] px-[15px] py-[8px] cursor-pointer'
+                    className="shadow-[0px_0px_4px_rgba(0,_0,_0,_0.25)] rounded-[10px] bg-white flex flex-col w-[320px] gap-[6px] px-[13px] py-[10px] cursor-pointer outline-none hover:outline-[#4341EA] hover:outline-solid hover:shadow-none"
                     onClick={() => selectPlace(result)}
                   >
-                    <span className='text-[13px] font-bold flex gap-[10px] items-center'>
+                    <span className='text-[15px] font-medium flex gap-[10px] items-center'>
                       {result.place_name}
                       <span className='text-[9px] font-light'>
                         {result.category_group_name}
                       </span>
                     </span>
-                    <span className='text-[10px] text-gray-600'>
+                    <span className='text-sub text-black opacity-80'>
                       {result.address_name}
                     </span>
-                    <span className='text-[10px] text-gray-600'>
+                    <span className='text-sub text-black opacity-80'>
                       {result.road_address_name}
                     </span>
                   </div>
