ed2740b23bcbc6c3d6fc7804df98267194c7a723
HidenLee <hyundo1995@gmail.com>
2025-04-24 03:59:20 +0900

    feat: clicking a push message will lead to target page

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,78 +1,75 @@
 importScripts(
-    "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"
-  );
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js'
+)
 importScripts(
-    "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js"
-  );
-  
-  self.addEventListener("install", function (e) {
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", function (e) {});
-  
-  self.addEventListener("notificationclick", function (event) {
-    event.notification.close();
-    const targetUrl = event.notification.data?.url;
-  
-    if (!targetUrl) return;
-  
-    event.waitUntil(
-      clients
-        .matchAll({ type: "window", includeUncontrolled: true })
-        .then((clientList) => {
-          for (const client of clientList) {
-            if (client.url.includes(targetUrl) && "focus" in client) {
-              return client.focus();
-            }
-          }
-          if (clients.openWindow) {
-            return clients.openWindow(targetUrl);
-          }
-        })
-    );
-  });
-  
-const firebaseConfig = {
-    apiKey: "AIzaSyDoedunVjEmdmVraM_L9xrN6HNKMinue7o",
-    authDomain: "wooco-d72ba.firebaseapp.com",
-    projectId: "wooco-d72ba",
-    storageBucket: "wooco-d72ba.firebasestorage.app",
-    messagingSenderId: "48093061821",
-    appId: "1:48093061821:web:4aac24e5a09e82625b8dd4",
-    measurementId: "G-R5NZNL4Z67"
-  };
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js'
+)
 
-  firebase.initializeApp(firebaseConfig);
-  
-  const messaging = firebase.messaging();
-  
-  messaging.onBackgroundMessage((payload) => {
-    const { title, body } = payload.notification || {};
-    const url = payload.data?.url;
-  
-    const options = {
-      badge: "/logo.png",
-      image: "/logo.png",
-      body: body || "새로운 메시지가 도착했어요.",
-      icon: "/logo.png",
-      data: url ? { url } : {},
-    };
-  
-    self.registration.showNotification(title || "알림", options);
-  });
-  
-  // install event
-  self.addEventListener("install", (e) => {
-    console.log("[Service Worker] installed");
-  });
-  
-  // activate event
-  self.addEventListener("activate", (e) => {
-    console.log("[Service Worker] actived", e);
-  });
-  
-  // fetch event
-  self.addEventListener("fetch", (e) => {
-    console.log("[Service Worker] fetched resource " + e.request.url);
-  });
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close()
+  const targetUrl = event.notification.data?.url
+
+  if (!targetUrl) return
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url.includes(targetUrl) && 'focus' in client) {
+            return client.focus()
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(targetUrl)
+        }
+      })
+  )
+})
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDoedunVjEmdmVraM_L9xrN6HNKMinue7o',
+  authDomain: 'wooco-d72ba.firebaseapp.com',
+  projectId: 'wooco-d72ba',
+  storageBucket: 'wooco-d72ba.firebasestorage.app',
+  messagingSenderId: '48093061821',
+  appId: '1:48093061821:web:4aac24e5a09e82625b8dd4',
+  measurementId: 'G-R5NZNL4Z67',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload.data?.title
+  const body = payload.data?.body
+
+  // const notification_id = payload.data?.notification_id;
+  // const user_id = payload.data?.user_id;
+  const target_id = payload.data?.target_id
+  const target_name = payload.data?.target_name
+  const type = payload.data?.type
+
+  const contentMap = {
+    COURSE_COMMENT_CREATED: `[${target_name}]에 새로운 댓글이 달렸어요!`,
+    PLAN_SHARE_REQUEST: `[${target_name}]가 좋았다면 사람들에게 공유해주세요`,
+    PLACE_REVIEW_REQUEST: `[${target_name}]에 대한 장소 리뷰 기다리고 있어요!`,
+    SYSTEM: `일상 속 어디든지 나만의 경로로, 좋은 공간 함께 나누고 공유해요`,
+  }
+  const urlMap = {
+    COURSE_COMMENT_CREATED: 'courses/',
+    PLAN_SHARE_REQUEST: 'plans/',
+    PLACE_REVIEW_REQUEST: 'places/',
+    SYSTEM: '',
+  }
+
+  const url = 'https://wooco.kr/' + urlMap[type] + target_id
+  const options = {
+    badge: '/logo_gradation.svg',
+    body: contentMap[type] || body || '새로운 메시지가 도착했어요.',
+    data: { url },
+  }
+
+  self.registration.showNotification(title || '알림', options)
+})
--- src/features/firebase/firebase-cloud-message.tsx
+++ src/features/firebase/firebase-cloud-message.tsx
@@ -1,60 +1,78 @@
-import { postDeviceToken } from "@/src/entities/notification";
-import { app } from '@/firbase';
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { postDeviceToken } from '@/src/entities/notification'
+import { app } from '@/firbase'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
 
 export const initFCM = () => {
-  const messaging = getMessaging(app);
-  const loginToken = localStorage.getItem("accessToken");
+  const messaging = getMessaging(app)
+  const loginToken = localStorage.getItem('accessToken')
   const requestPermissionAndGetToken = async () => {
     try {
-      const permission = await Notification.requestPermission();
-      if (permission === "granted") {
+      const permission = await Notification.requestPermission()
+      if (permission === 'granted') {
         const fcmToken = await getToken(messaging, {
           vapidKey: process.env.NEXT_FIREBASE_VAPID_KEY,
-        });
-
+        })
         if (fcmToken && loginToken) {
-          await postDeviceToken({token: fcmToken
-        });
+          await postDeviceToken({ token: fcmToken })
         } else {
-          console.log("FCM 토큰을 가져올 수 없습니다.");
+          console.log('FCM 토큰을 가져올 수 없습니다.')
         }
       } else {
-        console.log("알림 권한이 거부되었습니다.");
+        console.log('알림 권한이 거부되었습니다.')
       }
     } catch (error) {
-      console.error("FCM 토큰 요청 실패:", error);
+      console.error('FCM 토큰 요청 실패:', error)
     }
-  };
-  if ("serviceWorker" in navigator) {
-    (async () => {
+  }
+  if ('serviceWorker' in navigator) {
+    ;(async () => {
       try {
-        await navigator.serviceWorker.register("/firebase-messaging-sw.js");
-        await requestPermissionAndGetToken();
+        await navigator.serviceWorker.register('/firebase-messaging-sw.js')
+        await requestPermissionAndGetToken()
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    })();
+    })()
   }
 
   onMessage(messaging, (payload) => {
-    if (Notification.permission === "granted") {
-      const { title, body } = payload.notification || {};
-      const url = payload.data?.url;
+    if (Notification.permission === 'granted') {
+      const title = payload.data?.title
+      const body = payload.data?.body
 
-      const notification = new Notification(title || "알림", {
-        badge: "/logo.png",
-        body: body || "새로운 메시지가 도착했어요.",
-        icon: "/logo.png",
-        data: url ? { url } : {},
-      });
+      // const notification_id = payload.data?.notification_id;
+      // const user_id = payload.data?.user_id;
+      const target_id = payload.data?.target_id
+      const target_name = payload.data?.target_name
+      const type = payload.data?.type
+
+      const contentMap = {
+        COURSE_COMMENT_CREATED: `[${target_name}]에 새로운 댓글이 달렸어요!`,
+        PLAN_SHARE_REQUEST: `[${target_name}]가 좋았다면 사람들에게 공유해주세요`,
+        PLACE_REVIEW_REQUEST: `[${target_name}]에 대한 장소 리뷰 기다리고 있어요!`,
+        SYSTEM: `일상 속 어디든지 나만의 경로로, 좋은 공간 함께 나누고 공유해요`,
+      }
+      const urlMap = {
+        COURSE_COMMENT_CREATED: 'courses/',
+        PLAN_SHARE_REQUEST: 'plans/',
+        PLACE_REVIEW_REQUEST: 'places/',
+        SYSTEM: '',
+      }
+
+      const url = 'https://wooco.kr/' + urlMap[type] + target_id
+
+      const notification = new Notification(title || '알림', {
+        badge: '/logo_gradation.svg',
+        body: contentMap[type] || body || '새로운 메시지가 도착했어요.',
+        data: { url },
+      })
 
       notification.onclick = (event) => {
-        event.preventDefault();
+        event.preventDefault()
         if (notification.data?.url) {
-          window.open(notification.data.url, "_blank");
+          window.open(notification.data.url, '_blank')
         }
-      };
+      }
     }
-  });
-};
+  })
+}
