1ef0bb6a925edf25339983bb76936e3337462baa
yoouung <jyp101400@gmail.com>
2025-05-17 15:44:44 +0900

    refactor: remove unused codelines

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { ToastProvider, Spacer } from '@/src/shared/ui'
+import { Spacer } from '@/src/shared/ui'
 import { MainHeader } from '@/src/widgets'
 import DefaultFooter from '@/src/widgets/default-footer'
 import localFont from 'next/font/local'
-import { AnimatePresence } from 'framer-motion'
-import { ConfigProvider } from 'antd'
-import {
-  AppProviders,
-  AuthProvider,
-  ReactQueryProvider,
-} from '@/src/shared/provider'
-import { MessageProvider } from '@/src/shared/lib'
+import { AppProviders } from '@/src/shared/provider'
 
 export const metadata: Metadata = {
   title: 'WOOCO - 우코',
@@ -27,17 +20,6 @@ const pretendard = localFont({
   weight: '45 920',
   variable: '--font-pretendard',
 })
-
-const theme = {
-  token: {
-    borderRadius: 10,
-    colorPrimary: '#5A59F2',
-    colorBgContainer: '#F7F7F7',
-    colorBorder: '#ffffff',
-    colorText: 'rgba(0, 0, 0, 0.5)',
-    fontSize: 11,
-  },
-}
 
 export default function RootLayout({
   children,
