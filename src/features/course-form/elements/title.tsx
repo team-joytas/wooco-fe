6f7d93a16bf63d9e40e051f45b74a8e9ba0444ae
yoouung <jyp101400@gmail.com>
2025-05-20 16:28:35 +0900

    feat: resize input, textarea form not to be zoom on ios mobile

--- src/features/course-form/elements/title.tsx
+++ src/features/course-form/elements/title.tsx
@@ -22,7 +22,7 @@ export function FormTitle({
         {...register('title', { validate: validateTitle })}
         maxLength={20}
         placeholder='제목을 입력해주세요.'
-        className='rounded-full focus:outline-container-light-blue focus:outline-[0.5px] text-sub text-gray-500 h-[35px] border-0 bg-bright-gray px-[10px]'
+        className='rounded-full focus:outline-wooco_blue-primary-light focus:outline-[0.5px] text-main01 text-gray-800 border-0 bg-gray-100 px-[15px] scale-[0.875] h-[40px] w-[114.29%] origin-top-left'
       />
       {errors.title && <HelperText message={errors.title.message || ''} />}
     </>
