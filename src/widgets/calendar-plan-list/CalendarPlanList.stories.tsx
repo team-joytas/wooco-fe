268e3a525496231950032f9681a46facb5b24174
HidenLee <hyundo1995@gmail.com>
2025-11-04 12:28:21 +0900

    test: stories for calendar components

--- src/widgets/calendar-plan-list/CalendarPlanList.stories.tsx
+++ src/widgets/calendar-plan-list/CalendarPlanList.stories.tsx
@@ -1,19 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/nextjs'
-// import {CalendarPlanType} from '@/src/entities/calendar/model'
 import CalendarPlanList from './CalendarPlanList'
 
 const meta: Meta<typeof CalendarPlanList> = {
   title: 'Components/CalendarPlanList',
   component: CalendarPlanList,
-  parameters: {
-    layout: 'centered',
-  },
 }
 export default meta
 
 type Story = StoryObj<typeof CalendarPlanList>
 
 export const Default: Story = {
-
+  render:() =>
+    <CalendarPlanList data={[
+      {
+        title: "test",
+        groupName: "그룹1",
+        groupColor: "#3D3D3D",
+        /** Place info */
+        place: [],
+        /** How many peoples on the group? */
+        groupSize: 1,
+        /** Date */
+        date: "2025-10-28",
+      }]} />
 }
 
